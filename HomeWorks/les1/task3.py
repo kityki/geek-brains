8267f082baa1f05bcec58908e9dc3be8cd8c8f49
Kris <kristina.benyaminova@gmail.com>
2020-10-26 14:50:46 +0100

    task3 first

--- HomeWorks/les1/task3.py
+++ HomeWorks/les1/task3.py
@@ -1,12 +1,11 @@
 print('Greetings, user!')
 
 number = input('Enter a number.\n>>>')
-result = f'{number} + {number*2} + {number*3}'
-print(result)
 
-"""
-number = input('Enter a number.\n>>>')
-i = 0
+numb1 = int(number)
+numb2 = int(number*2)
+numb3 = int(number*3)
 
-for 
-"""
+res = numb1 + numb2 + numb3
+
+print(f'{number} + {number*2} + {number*3} = {res}.')
