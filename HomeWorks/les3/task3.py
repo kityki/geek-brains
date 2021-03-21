b4700c0dbe8ecf23cc151a61a8f26065677993dc
Kris <kristina.benyaminova@gmail.com>
2021-03-22 00:23:16 +0100

    lesson 3

--- HomeWorks/les3/task3.py
+++ HomeWorks/les3/task3.py
@@ -2,7 +2,7 @@
 3. Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь,
 в котором ключи — первые буквы имен, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы.
 Например:
->>> >>> thesaurus("Иван", "Мария", "Петр", "Илья")
+#>>> >>> thesaurus("Иван", "Мария", "Петр", "Илья")
 {
     "И": ["Иван", "Илья"],
     "М": ["Мария"], "П": ["Петр"]
@@ -10,28 +10,21 @@
 Подумайте: полезен ли будет вам оператор распаковки? Сможете ли вы вернуть отсортированный по ключам словарь?
 """
 
+def thesaurus(*args):
+
+    dict_names = {}
+
+    for name in args:
+        first_letter = name[0]
+        dict_names.setdefault(first_letter, [])
+        dict_names[first_letter].append(name)
+        return dict_names
 
 
-
-"""
-Реализовать функцию my_func(), которая принимает три позиционных аргумента,
-и возвращает сумму наибольших двух аргументов.
-"""
-
-def my_func(num_1, num_2, num_3):
-    num_1, num_2, num_3 = int(num_1), int(num_2), int(num_3)
-    n = [num_1, num_2, num_3]
-    for i in n:
-        if i < num_1 and i < num_2:
-            summ = num_1 + num_2
-            return summ
-        elif i < num_1 and i < num_3:
-            summ = num_1 + num_3
-            return summ
-        elif i < num_2 and i < num_3:
-            summ = num_2 + num_3
-            return summ
+d = thesaurus("Иван", "Мария", "Петр", "Илья")
+for first_letter in sorted(d.keys()):
+    print(f'{first_letter}: {d[first_letter]}')
 
 
-
-print(my_func(8, 45, 543))
+# мое собственное решение не получилось, попробовала Ваше, но он выдал только: {'И': ['Иван']}, хотя в Вашем решении все
+# сработало. Почему так? У меня верисия питона 3.7, PyCharm 2018.
